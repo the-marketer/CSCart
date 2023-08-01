6c225222162c0ff147cc630b87b86101425954b4
EAX LEX <lex@eax.ro>
2023-08-01 16:22:15 +0300

    bun

--- app/addons/themarketer/schemas/menu/menu.post.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$schema['top']['addons']['items']['themarketer_settings'] = array(
-    'href' => 'addons.update&addon=themarketer',
-    'position' => 500
-);
-return $schema;
--- design/themes/responsive/templates/addons/themarketer/hooks/index/footer.post.tpl
+++
@@ -1,5 +0,0 @@
-<script>
-	// wishilist add
-	{if $tm_add_wishlist}{$tm_add_wishlist nofilter}{/if}
-</script>
-{$order_datalayer}111
